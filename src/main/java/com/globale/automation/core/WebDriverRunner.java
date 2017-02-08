703ac3b140d7090a1be146f24b6ea2ba33dc8582
dimam <dima.masis@checkmarx.com>
2017-02-08 19:56:41 +0200

    test

---
+++ src/main/java/com/globale/automation/core/WebDriverRunner.java
@@ -0,0 +1,8 @@
+package com.globale.automation.core;
+
+/**
+ * Created by Dima Masis
+ * Date 08/02/2017
+ */
+public class WebDriverRunner {
+}
---
+++ src/test/java/com/globale/automation/AnnaWebSiteTest.java
@@ -0,0 +1,16 @@
+package com.globale.automation;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Dima Masis
+ * Date 08/02/2017
+ */
+public class AnnaWebSiteTest {
+
+    @Test
+    public void searchForAutomationInGoogle() {
+        System.out.println("hello");
+    }
+
+}
